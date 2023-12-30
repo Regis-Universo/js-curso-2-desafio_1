fae90f79b57bfead46de6a0fdb55adb329cf726f
Regis <regisuniverso@gmail.com>
2023-12-29 21:57:49 -0300

    commit final

--- README.md
+++ README.md
@@ -3,16 +3,19 @@ Em uma carreira de desenvolvimento de software, a prática consistente da lógic
 A lógica de programação não apenas permite a criação de algoritmos eficientes e soluções elegantes, mas também desenvolve a capacidade de pensar de forma estruturada e analítica.Essa habilidade é essencial para enfrentar desafios complexos e transformar problemas abstratos em implementações tangíveis.
 
 Pensando nisso, criamos uma lista de atividades (não obrigatórias) focada em prática para melhorar ainda mais sua experiência de aprendizagem. Bora praticar então?
-Desafios
+Desafios com comentários
+DESAFIOS COMENTADOS
 
-    Faça o download de outro projeto clicando neste link e abra no Visual Studio Code.
+1. Faça o download de outro projeto clicando neste link e abra no Visual Studio Code.
+   https://github.com/alura-cursos/js-curso-2.git
 
-    Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+2. Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
 
-    Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+3. Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
 
-    Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+4. Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
 
-    Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+5. Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
 
-    Ao clicar no botão soma, peça 2 números e exiba o resultado da soma em um alerta.
+6. Ao clicar no botão soma, peça 2 números e exiba o resultado da soma em um alerta.
+
--- app.js
+++ app.js
@@ -1,12 +1,56 @@
-/*2. Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.*/
+// 1. Faça o download de outro projeto clicando neste link e abra no Visual Studio Code. 
+// https://github.com/alura-cursos/js-curso-2.git
+
+
+/* 2. Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio. */
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+/* 3. Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado. */
+
+function verificarConsole() {
+  console.log('O botão foi clicado');
+}
+
+
+/* 4. Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado. */
+
+function verificarAlerta() {
+  alert('Eu amo JavaScript');
+}
+
+/* 5. Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você. */
+
+function verificarPrompt() {
+  let nomeDacidade = prompt('Em que cidade você esteve?');
+
+  alert(` Estive em ${nomeDacidade}, e lembrei de você!`);
+}
 
 
 
-/*Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+// 6. Ao clicar no botão soma, peça 2 números e exiba o resultado da soma em um alerta.
+ 
 
-Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+/* Utilizando a function Number para transformar uma variável que veio do prompt, do tipo string em tipo number no JavaScript, podemos usar a função Number(). A função Number() converte uma string para um número inteiro ou um número com casas decimais.*/
 
-Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+function somar() {
+  alert('Essa função soma 2 números e exibe o resultado da soma em um alerta');
+  let numero1 = Number(prompt('Digite o primeiro número: '));
+  let numero2 = Number(prompt('Digite o segundo número'));
+  let resultado = numero1 + numero2;
+  alert(`A soma de ${numero1} + ${numero2} é igual a ${resultado}`);
+}
 
-Ao clicar no botão soma, peça 2 números e exiba o resultado da soma em um alerta.
- */
+/* Outra forma de converter uma variável que veio do prompt, do tipo string em tipo number no JavaScript, é usar a função parseInt(). A função parseInt() converte uma string para um número inteiro.*/
+
+// function somar() {
+
+//   alert('Essa função soma 2 números e exibe o resultado da soma em um alerta');
+ 
+//   let numero1 = parseInt(prompt('Digite o primeiro número: '));
+//   let numero2 = parseInt(prompt('Digite o segundo número'));
+//   let resultado = numero1 + numero2;
+//   alert(`A soma de ${numero1} + ${numero2} é igual a ${resultado}`);
+// }
